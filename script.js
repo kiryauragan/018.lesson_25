3b236b7bef51d245a1ed159fdd6615b44945e793
kiryauragan <you@example.comkirilltaranskyi@gmail.com>
2023-04-09 17:20:19 +0300

    task updated

--- script.js
+++ script.js
@@ -5,28 +5,32 @@ let votes = new Array(emojis.length).fill(0);
 const buttonsContainer = document.createElement("divBtns");
 const votesContainer = document.createElement("divVotes");
 
+buttonsContainer.addEventListener("click", function (event) {
+	if (event.target.matches("button")) {
+		const buttonIndex = emojis.indexOf(event.target.innerText);
+		votes[buttonIndex]++;
+		updateVotesDisplay();
+	}
+});
+
 for (let i = 0; i < emojis.length; i++) {
-  const button = document.createElement("button");
+	const button = document.createElement("button");
 
-  button.classList.add("emoji-button");
-
-  button.innerText = emojis[i];
-
-  buttonsContainer.appendChild(button);
-
-  button.addEventListener("click", function () {
-    votes[i]++;
-    updVotesDisplay();
-  });
+	button.classList.add("emoji-button");
+	button.innerText = emojis[i];
+	buttonsContainer.appendChild(button);
 }
+
+updateVotesDisplay();
 
 document.body.appendChild(buttonsContainer);
 document.body.appendChild(votesContainer);
 
-function updVotesDisplay() {
-  let displayString = "";
-  for (let i = 0; i < emojis.length; i++) {
-    displayString += `${emojis[i]}: ${votes[i]} `;
-  }
-  votesContainer.innerText = displayString;
+function updateVotesDisplay() {
+	votesContainer.innerHTML = "";
+	for (let i = 0; i < emojis.length; i++) {
+		const span = document.createElement("span");
+		span.innerText = `${emojis[i]}: ${votes[i]}`;
+		votesContainer.appendChild(span);
+	}
 }
